cabb9ff2f4cb8e14277da92fa23d4a1e79c42b20
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-06-02 15:22:55 +0700

    video17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    
+    state ={
+        name :"Tien",
+        address: "123",
+        age: 20,
+        gender: "Male"
+    };
+
     //JSX
     render() {
         return (
             <div>
                 <h1>Hello World</h1>
-                {Math.random()}
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         );
 }
